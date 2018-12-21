0a66dfe5cdf24f7c71d1a98c8e06565e6e1e6408
Marta <marta.albets@gmail.com>
2018-12-21 09:13:34 +0100

    Tests

--- test/LinkedBinarySearchTreeTest.java
+++ test/LinkedBinarySearchTreeTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -12,17 +14,58 @@ public class LinkedBinarySearchTreeTest<K, V> {
     @Test
     public void put() {
         LinkedBinarySearchTree<Integer, Long> lbst = new LinkedBinarySearchTree<Integer, Long>(Comparator.naturalOrder());
-        LinkedBinarySearchTree<Integer, Long> expectedLbst = lbst;
-        try {
-            lbst.put(3, null);
-        } catch(NullPointerException ex){
-            System.out.println("Catch exception error: " + numException);
-        }
-        assertEquals(expectedLbst.toString(), lbst.toString());
-        lbst = lbst.put(3, 2L);
-        assertTrue(lbst.containsKey(3));
-
+        ArrayList<Pair<Integer, Long>> expectedList = new ArrayList<>();
+        lbst = lbst.put(8, 8L);
+        lbst = lbst.put(12, 12L);
+        lbst = lbst.put(7, 7L);
+        lbst = lbst.put(15, 15L);
+        assertEquals("(7, 7L), (8, 8L), (12, 12L), (15, 15L)", lbst.toString());
     }
+
+    @Test (expected = NullPointerException.class)
+    public void putNullKey(){
+        LinkedBinarySearchTree<Integer, Long> lbst = new LinkedBinarySearchTree<Integer, Long>(Comparator.naturalOrder());
+        lbst.put(null, 8L);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void putNullValue(){
+        LinkedBinarySearchTree<Integer, Long> lbst = new LinkedBinarySearchTree<Integer, Long>(Comparator.naturalOrder());
+        lbst.put(6, null);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void putNullKeyAndNullValue(){
+        LinkedBinarySearchTree<Integer, Long> lbst = new LinkedBinarySearchTree<Integer, Long>(Comparator.naturalOrder());
+        lbst.put(null, null);
+    }
+
+    @Test
+    public void remove(){
+        LinkedBinarySearchTree<Integer, Long> lbst = new LinkedBinarySearchTree<Integer, Long>(Comparator.naturalOrder());
+            lbst.put(8, 8L);
+            lbst.put(12, 12L);
+            lbst.put(7, 7L);
+            lbst.put(15, 15L);
+            lbst.put(5, 5L);
+            lbst.put(2, 2L);
+            lbst.remove(5);
+            lbst.remove(2);
+            assertEquals("(7, 7L), (8, 8L), (12, 12L), (15, 15L)", lbst.toString());
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void removeNullKey(){
+        LinkedBinarySearchTree<Integer, Long> lbst = new LinkedBinarySearchTree<Integer, Long>(Comparator.naturalOrder());
+        lbst.put(8, 8L);
+        lbst.put(12, 12L);
+        lbst.put(7, 7L);
+        lbst.put(15, 15L);
+        lbst.put(5, 5L);
+        lbst.put(2, 2L);
+        lbst.remove(null);
+    }
+
 
     @Test
     public void isEmpty() {
@@ -44,19 +87,18 @@ public class LinkedBinarySearchTreeTest<K, V> {
         assertTrue(lbst.containsKey(3));
         assertTrue(lbst.containsKey(0));
         assertTrue(lbst.containsKey(4));
-        try{
-            lbst = lbst.put(6, null);
-        } catch(NullPointerException ex){
-
-        }
-        assertFalse(lbst.containsKey(6));
+        assertFalse(lbst.containsKey(5));
     }
 
-
-
     @Test
-    public void inOrder() {
+    public void inOrder(){
         LinkedBinarySearchTree<Integer, Long> lbst = new LinkedBinarySearchTree<Integer, Long>(Comparator.naturalOrder());
+        ArrayList<Pair<Integer, Long>> expectedList = new ArrayList<>();
+        long l = 0;
+        for(int i = 0; i < 7; i++){
+            expectedList.add(new Pair<>(i, l));
+            l++;
+        }
         lbst = lbst.put(3,3L);
         lbst = lbst.put(4, 4L);
         lbst = lbst.put(1, 1L);
@@ -64,8 +106,8 @@ public class LinkedBinarySearchTreeTest<K, V> {
         lbst = lbst.put(2,2L);
         lbst = lbst.put(6,6L);
         lbst = lbst.put(5,5L);
-        System.out.println(InOrder.inOrder(lbst).toString());
-        System.out.println("Hola");
+        assertEquals(InOrder.inOrder(lbst), expectedList);
+
     }
 
 }
